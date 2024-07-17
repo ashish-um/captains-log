138932e2af26f86f05b49bedd50e63288d4b6281
ashish-um <ashishumrao632005@gmail.com>
2024-07-17 17:07:57 +0530

    loading added

---
+++ src/app/loading.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+export default function loading() {
+  return (
+    <img
+      width={100}
+      src="spinner.svg"
+      className="mx-auto mt-20"
+      alt="loading..."
+    />
+  );
+}
