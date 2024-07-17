91f2a631d3a4f034effb969224ab0069c6a16627
ashish-um <ashishumrao632005@gmail.com>
2024-07-17 23:51:28 +0530

    pretty css

--- src/app/page.js
+++ src/app/page.js
@@ -2,10 +2,10 @@ import Card from "@/components/Card";
 import WatchToOmdb from "@/utils/watch_to_omdb";
 
 export default async function page() {
-  const dat = await WatchToOmdb();
+  const data = await WatchToOmdb();
   console.log(data);
   return (
-    <div className="grid-cols-2 grid sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 2xl:grid-cols-5 max-w-screen-2xl mx-auto py-4">
+    <div className="grid-cols-2 grid sm:grid-cols-3 lg:grid-cols-4 xl:grid-cols-5 max-w-screen-2xl mx-auto py-4">
       {data.map((item) => {
         return <Card key={item.imdbID} data={item} />;
       })}
--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -2,7 +2,7 @@ import Image from "next/image";
 
 export default function Card({ data }) {
   return (
-    <div>
+    <div className="group flex flex-col relative">
       <Image
         src={data.Poster}
         alt="poster"
@@ -10,6 +10,36 @@ export default function Card({ data }) {
         width={500}
         height={675}
       />
+
+      <div className="absolute top-2 right-2 flex flex-col gap-1">
+        <span
+          className={`uppercase ${
+            data.Type === "movie" ? "bg-blue-700" : "bg-blue-400"
+          } px-2 py-1 rounded-md shadow-sm shadow-black`}
+        >
+          {data.Type}
+        </span>
+        {data.imdbRating !== "N/A" && (
+          <span
+            className={`${
+              Number(data.imdbRating) > 7 ? "bg-green-500" : "bg-red-700"
+            } px-2 py-1 rounded-md shadow-sm shadow-black`}
+          >
+            {data.imdbRating}
+            <span className="material-symbols-rounded text-base pl-2">
+              {Number(data.imdbRating) > 7 ? "trending_up" : "trending_down"}
+            </span>
+          </span>
+        )}
+      </div>
+
+      <h3 className="text-lg font-bold pr-4 inline lg:hidden">{data.Title}</h3>
+      <div className="opacity-0 inset-0 absolute lg:group-hover:opacity-100 select-none transition-all flex justify-center text-center items-center before:content-[''] before:bg-black before:block before:absolute before:inset-0 before:opacity-70 ">
+        <div className="relative px-3">
+          <h3 className="text-lg font-bold">{data.Title}</h3>
+          <p>{data.Year}</p>
+        </div>
+      </div>
     </div>
   );
 }
