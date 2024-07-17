5a4dfaacefb6da591833e6dc87f15c2793a0fe77
ashish-um <ashishumrao632005@gmail.com>
2024-07-17 16:43:54 +0530

    error handled

---
+++ src/app/error.js
@@ -0,0 +1,27 @@
+"use client";
+import { useEffect } from "react";
+
+export default function error({ error, reset }) {
+  useEffect(() => {
+    console.error(error);
+  }, [error]);
+
+  return (
+    <div className="text-center mt-32 mx-10">
+      <img
+        className="mx-auto"
+        width={100}
+        src="https://media.tenor.com/a7qa0Uk3F30AAAAi/peach-goma-peach-and-goma.gif"
+      />
+      <h2 className="text-xl font-bold text-red-400 my-4">
+        Something went wrong. Please try again later.
+      </h2>
+      <button
+        onClick={() => reset()}
+        className="bg-emerald-500 px-2 py-1 rounded-md font-bold hover:bg-emerald-600 transition-all duration-100"
+      >
+        Try Again
+      </button>
+    </div>
+  );
+}
