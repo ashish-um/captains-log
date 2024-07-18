4e3e1574db776106798b11377e0b08b835c29f0d
ashish-um <ashishumrao632005@gmail.com>
2024-07-18 11:51:28 +0530

    pretty css mobile

--- src/app/page.js
+++ src/app/page.js
@@ -5,7 +5,7 @@ export default async function page() {
   const data = await WatchToOmdb();
   console.log(data);
   return (
-    <div className="grid-cols-2 grid sm:grid-cols-3 lg:grid-cols-4 xl:grid-cols-5 max-w-screen-2xl mx-auto py-4">
+    <div className="gap-3 px-3 grid-cols-2 grid sm:grid-cols-3 lg:grid-cols-4 xl:grid-cols-5 max-w-screen-2xl mx-auto py-4">
       {data.map((item) => {
         return <Card key={item.imdbID} data={item} />;
       })}
--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -11,11 +11,11 @@ export default function Card({ data }) {
         height={675}
       />
 
-      <div className="absolute top-2 right-2 flex flex-col gap-1">
+      <div className="absolute top-2 right-2 flex flex-col gap-1 opacity-90">
         <span
           className={`uppercase ${
-            data.Type === "movie" ? "bg-blue-700" : "bg-blue-400"
-          } px-2 py-1 rounded-md shadow-sm shadow-black`}
+            data.Type === "movie" ? "bg-blue-800" : "bg-amber-600"
+          } px-2 py-1 rounded-md shadow-sm shadow-black text-xs sm:text-lg font-semibold border-2`}
         >
           {data.Type}
         </span>
@@ -23,18 +23,20 @@ export default function Card({ data }) {
           <span
             className={`${
               Number(data.imdbRating) > 7 ? "bg-green-500" : "bg-red-700"
-            } px-2 py-1 rounded-md shadow-sm shadow-black`}
+            } px-2 py-0.5 rounded-md shadow-sm shadow-black text-sm sm:py-1 sm:text-lg flex items-center border-2`}
           >
             {data.imdbRating}
-            <span className="material-symbols-rounded text-base pl-2">
+            <span className="material-symbols-rounded text-xs sm:text-base pl-2">
               {Number(data.imdbRating) > 7 ? "trending_up" : "trending_down"}
             </span>
           </span>
         )}
       </div>
 
-      <h3 className="text-lg font-bold pr-4 inline lg:hidden">{data.Title}</h3>
-      <div className="opacity-0 inset-0 absolute lg:group-hover:opacity-100 select-none transition-all flex justify-center text-center items-center before:content-[''] before:bg-black before:block before:absolute before:inset-0 before:opacity-70 ">
+      <h3 className="text-lg font-bold inline lg:hidden absolute bottom-0 bg-slate-800 w-full p-1 text-nowrap overflow-scroll">
+        {data.Title}
+      </h3>
+      <div className="opacity-0 inset-0 absolute lg:group-hover:opacity-100 transition-all flex justify-center text-center items-center before:content-[''] before:bg-black before:block before:absolute before:inset-0 before:opacity-70 ">
         <div className="relative px-3">
           <h3 className="text-lg font-bold">{data.Title}</h3>
           <p>{data.Year}</p>
