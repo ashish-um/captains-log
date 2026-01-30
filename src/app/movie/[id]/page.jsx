29e6fcdb89827f3c35b40314f08471b7b50e69bf
Ashish Umrao <ashishumrao632005@gmail.com>
2026-01-30 11:25:33 +0530

    redirect to external website for watching

--- src/app/movie/[id]/page.jsx
+++ src/app/movie/[id]/page.jsx
@@ -1,17 +1,25 @@
 import getIframe from "@/utils/iframe_url";
-
+import Link from "next/link";
+import Image from "next/image";
 export default async function page(movieID) {
   const id = movieID.params.id;
   const embedURL = await getIframe(id);
 
   return (
-    <div>
-      <iframe
-        src={embedURL}
-        allowFullScreen
-        width={"100%"}
-        style={{ height: "80svh" }}
-      ></iframe>
+    <div className="flex items-center justify-center">
+      <Link
+        className="flex items-center gap-2 mt-20 bg-emerald-500 px-4 py-3 rounded-md font-bold hover:bg-emerald-600"
+        href={embedURL}
+        target="_blank"
+      >
+        <p className="pt-0.5 text-2xl">Watch</p>
+        <Image
+          width={40}
+          height={40}
+          alt="trending status"
+          src={"/link.svg"}
+        />
+      </Link>
     </div>
   );
 }
--- src/app/series/[id]/[season]/[episode]/page.jsx
+++ src/app/series/[id]/[season]/[episode]/page.jsx
@@ -1,4 +1,6 @@
 import getIframe from "@/utils/iframe_url";
+import Link from "next/link";
+import Image from "next/image";
 
 export default async function page(routes) {
   const id = routes.params.id;
@@ -7,13 +9,28 @@ export default async function page(routes) {
   const embedURL = await getIframe(id, season, episode);
 
   return (
-    <div>
-      <iframe
-        src={embedURL}
-        allowFullScreen
-        width={"100%"}
-        className="h-[40svh] lg:h-[80vh]"
-      ></iframe>
+    <div className="flex items-center flex-col mt-10 justify-center gap-6">
+      <div>
+        <p className="text-xl">ID: {id}</p>
+        <br />
+        <p className="text-xl">Season: {season}</p>
+        <br />
+        <p className="text-xl">Episode: {episode}</p>
+      </div>
+
+      <Link
+        className="flex items-center gap-2 mb-6 bg-emerald-500 px-4 py-3 rounded-md font-bold hover:bg-emerald-600"
+        href={embedURL}
+        target="_blank"
+      >
+        <p className="pt-0.5 text-2xl">Watch</p>
+        <Image
+          width={40}
+          height={40}
+          alt="trending status"
+          src={"/link.svg"}
+        />
+      </Link>
     </div>
   );
 }
